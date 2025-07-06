d5a0d35466f749afb5660b06b706c8950b8ec07f
RaviGahire <ravigahire3@gmail.com>
2025-07-07 00:58:33 +0530

    added filter by region functionality on the main page

--- rest-countries-api-with-color-theme-switcher-master/main.js
+++ rest-countries-api-with-color-theme-switcher-master/main.js
@@ -1,22 +1,54 @@
-countryContainer = document.querySelector('.countries-container')
+const countryContainer = document.querySelector(".countries-container");
+const filterBar = document.querySelector(".filter-bar");
 
 fetch("./data.json")
   .then((res) => res.json())
   .then((data) => {
     data.forEach((country) => {
-      const countryCards= document.createElement('a')
-      countryCards.href = `/country.html?name=${country.name}`
-      countryCards.classList.add('country-card')
+      const countryCards = document.createElement("a");
+      countryCards.href = `/country.html?name=${country.name}`;
+      countryCards.classList.add("country-card");
       countryCards.innerHTML = `
                       <img src="${country.flags.svg}" alt="flag">
                 <div class="card-text">
                     <h3 class="card-title">${country.name}</h3>
-                    <p><b>Population: </b>${country.population.toLocaleString('en-IN')}</p>
+                    <p><b>Population: </b>${country.population.toLocaleString(
+                      "en-IN"
+                    )}</p>
                     <p><b>Region: </b>${country.region}</p>
-                    <p><b>Capital: </b>${country.capital || 'Capital Less'}</p>
+                    <p><b>Capital: </b>${country.capital || "Capital Less"}</p>
                    
                 </div>
-               `
-               countryContainer.append(countryCards)
+               `;
+      countryContainer.append(countryCards);
     });
   });
+
+filterBar.addEventListener("change", (event) => {
+  fetch("./data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      countryContainer.innerHTML = "";
+      
+      data.forEach((country) => {
+        const countryCards = document.createElement("a");
+        countryCards.href = `/country.html?name=${country.name}`;
+
+        countryCards.classList.add("country-card");
+        countryCards.innerHTML = `
+                    <img src="${country.flags.svg}" alt="flag">
+                    <div class="card-text">
+                    <h3 class="card-title">${country.name}</h3>
+                    <p><b>Population: </b>${country.population.toLocaleString(
+                      "en-IN"
+                    )}</p>
+                    <p><b>Region: </b>${country.region}</p>
+                    <p><b>Capital: </b>${country.capital || "Capital Less"}</p>
+                    </div>`;
+
+        if (country.region === filterBar.value) {
+          country.region = countryContainer.append(countryCards);
+        }
+      });
+    });
+});
