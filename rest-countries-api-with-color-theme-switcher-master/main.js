991694d8e5ada5aa8ba82293ae33c8cc6ba4dcfa
RaviGahire <ravigahire3@gmail.com>
2025-07-07 01:15:37 +0530

    tried to avoid code repetition

--- rest-countries-api-with-color-theme-switcher-master/main.js
+++ rest-countries-api-with-color-theme-switcher-master/main.js
@@ -3,33 +3,14 @@ const filterBar = document.querySelector(".filter-bar");
 
 fetch("./data.json")
   .then((res) => res.json())
-  .then((data) => {
-    data.forEach((country) => {
-      const countryCards = document.createElement("a");
-      countryCards.href = `/country.html?name=${country.name}`;
-      countryCards.classList.add("country-card");
-      countryCards.innerHTML = `
-                      <img src="${country.flags.svg}" alt="flag">
-                <div class="card-text">
-                    <h3 class="card-title">${country.name}</h3>
-                    <p><b>Population: </b>${country.population.toLocaleString(
-                      "en-IN"
-                    )}</p>
-                    <p><b>Region: </b>${country.region}</p>
-                    <p><b>Capital: </b>${country.capital || "Capital Less"}</p>
-                   
-                </div>
-               `;
-      countryContainer.append(countryCards);
-    });
-  });
+  .then(renderCountrise);
 
-filterBar.addEventListener("change", (event) => {
+filterBar.addEventListener("change", () => {
   fetch("./data.json")
     .then((res) => res.json())
     .then((data) => {
       countryContainer.innerHTML = "";
-      
+
       data.forEach((country) => {
         const countryCards = document.createElement("a");
         countryCards.href = `/country.html?name=${country.name}`;
@@ -52,3 +33,25 @@ filterBar.addEventListener("change", (event) => {
       });
     });
 });
+
+function renderCountrise(data) {
+  countryContainer.innerHTML = "";
+  data.forEach((country) => {
+    const countryCards = document.createElement("a");
+    countryCards.href = `/country.html?name=${country.name}`;
+    countryCards.classList.add("country-card");
+    countryCards.innerHTML = `
+                      <img src="${country.flags.svg}" alt="flag">
+                <div class="card-text">
+                    <h3 class="card-title">${country.name}</h3>
+                    <p><b>Population: </b>${country.population.toLocaleString(
+                      "en-IN"
+                    )}</p>
+                    <p><b>Region: </b>${country.region}</p>
+                    <p><b>Capital: </b>${country.capital || "Capital Less"}</p>
+                   
+                </div>
+               `;
+    countryContainer.append(countryCards);
+  });
+}
