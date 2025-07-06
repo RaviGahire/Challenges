bfb5af4d7741e366aa20cb6801d20b4ebac6590c
RaviGahire <ravigahire3@gmail.com>
2025-07-06 13:15:06 +0530

    Fetch the all data about one country

--- rest-countries-api-with-color-theme-switcher-master/country.js
+++ rest-countries-api-with-color-theme-switcher-master/country.js
@@ -1,3 +1,15 @@
-const countryName = new URLSearchParams(location.search).get('name');
+const countryName = new URLSearchParams(location.search).get("name");
 
-fetch(`./data.json`)
+console.log(countryName);
+
+fetch("./data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const country = data.find((conName) => conName.name === countryName);
+    if (country) {
+      console.log(country);
+    } else {
+      console.log("Country not found");
+    }
+  })
+  .catch(console.error);
