2cf6f20fb2cdb9f4d10c902044adfe371da6596c
RaviGahire <ravigahire3@gmail.com>
2025-07-06 17:51:55 +0530

    Added all necessary details on country page

--- rest-countries-api-with-color-theme-switcher-master/country.js
+++ rest-countries-api-with-color-theme-switcher-master/country.js
@@ -1,15 +1,44 @@
 const countryName = new URLSearchParams(location.search).get("name");
+const flagImg = document.querySelector(".country-details img");
+const countryNameh1 = document.querySelector(".country-details h1");
+const NativeName = document.querySelector(".nt-name");
+const population = document.querySelector(".population");
+const region = document.querySelector(".region");
+const subRegion = document.querySelector(".sub-region");
+const conCapital = document.querySelector(".con-capital");
+const domain = document.querySelector(".domain");
+const currencies = document.querySelector(".currencies");
+const languages = document.querySelector(".languages");
 
-console.log(countryName);
+
+// console.log(countryName);
 
 fetch("./data.json")
   .then((res) => res.json())
   .then((data) => {
-    const country = data.find((conName) => conName.name === countryName);
+    const country = data.find((contName) => contName.name === countryName);
     if (country) {
       console.log(country);
     } else {
       console.log("Country not found");
     }
+    // Looping over the one country and showing data on the webpage
+    flagImg.src = country.flags.svg;
+    countryNameh1.innerText = country.name;
+    if (NativeName) {
+      NativeName.innerText = country.nativeName;
+    } else {
+      NativeName.innerText = country.name;
+    }
+    population.innerText = country.population;
+    region.innerText = country.region;
+    subRegion.innerText = country.subregion || 'No Sub-Region';
+    conCapital.innerText = country.capital || 'Capital Less';
+    domain.innerText = country.topLevelDomain.join(', ') || '.com';
+    currencies.innerText = country.currencies[0].code || 'No Currencies ';
+    languages.innerText = country.languages[0].name || 'No languages' ;
+
+console.log(country.currencies)
+
   })
   .catch(console.error);
