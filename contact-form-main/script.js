856e92660e1afde2b77b9265672130c023f5d3a4
RaviGahire <ravigahire3@gmail.com>
2025-07-26 01:02:28 +0530

    all validation done

--- contact-form-main/script.js
+++ contact-form-main/script.js
@@ -3,13 +3,15 @@ const form = document.querySelector("form");
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  form.querySelectorAll('.error-label').forEach((err) => err.innerText = '')
+  // form.querySelectorAll('.error-label').forEach((err) => err.innerText = '')
 
 
-  form.querySelectorAll('input[type=text]').forEach((input) => {
+  form.querySelectorAll('input[type=text],textarea[name="message"]').forEach((input) => {
     const errorLabel = input.nextElementSibling;
     if (input.value.trim() === '') {
       errorLabel.innerText = 'This field is required';
+    } else {
+      errorLabel.innerText = '';
     }
 
   });
@@ -24,6 +26,27 @@ form.addEventListener('submit', (e) => {
   } else if (!emailRegex.test(email.value.trim())) {
     emailError.innerText = 'Please enter a valid email address';
   }
+  else {
+    emailError.innerText = '';
+  }
+
+
+  const isChecked = form.querySelector('input[name="option"]:checked');
+  const err = document.querySelector('.query-error')
+  if (!isChecked) {
+    err.innerText = 'Please select a query type'
+  } else {
+    err.innerText = ''
+  }
+
+const checkBox = form.querySelector('input[type=checkbox]:checked');
+  const err_box = document.querySelector('.check-box-error')
+if (!checkBox) {
+     err_box.innerText = 'To submit this form. please consent to being contacted'
+  } else {
+    err_box.innerText = ''
+  }
+
 
 })
 
