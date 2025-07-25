9bb3ba466061d58ede810e5b372691b0e9bc6f73
RaviGahire <ravigahire3@gmail.com>
2025-07-25 19:40:37 +0530

    Added validation on name and email

--- contact-form-main/script.js
+++ contact-form-main/script.js
@@ -1,7 +1,31 @@
-const form = document.querySelector('form');
+const form = document.querySelector("form");
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  form.querySelectorAll('.error-label').forEach((err) => err.innerText = '')
 
 
-form.addEventListener('submit',(e)=>{
+  form.querySelectorAll('input[type=text]').forEach((input) => {
+    const errorLabel = input.nextElementSibling;
+    if (input.value.trim() === '') {
+      errorLabel.innerText = 'This field is required';
+    }
+
+  });
+
+  const email = form.querySelector('input[type="email"]');
+  const emailError = email.nextElementSibling;
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (email.value.trim() === '') {
+    emailError.innerText = 'This field is required';
+  } else if (!emailRegex.test(email.value.trim())) {
+    emailError.innerText = 'Please enter a valid email address';
+  }
+
+})
 
 
 
@@ -18,7 +42,10 @@ form.addEventListener('submit',(e)=>{
 
 
 
-})// form End
+
+
+
+
 
 
 
